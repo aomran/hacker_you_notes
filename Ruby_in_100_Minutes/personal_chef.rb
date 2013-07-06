f3afe7ddb71273583f749c5d313af1f76a422627
vagrant <vagrant@precise64.(none)>
2013-07-06 15:35:46 +0000

    Finished the Ruby in 100 Minutes reading

--- Ruby_in_100_Minutes/personal_chef.rb
+++ Ruby_in_100_Minutes/personal_chef.rb
@@ -19,12 +19,47 @@ class PersonalChef
 
 # NOT UNDERSTOOD
 
-  def gameplan(meals)
-    meals.each do |meal|
-    puts "We'll have #{meal}..."
+  # def gameplan(meals)
+    # meals.each do |meal|
+    # puts "We'll have #{meal}..."
+  # end
+    # all_meals = meals.join(", ")
+    # puts "In summary: #{all_meals}"
+
+# Creating an inventory
+
+  def inventory
+  produce = {apples: 3, oranges: 1, carrots: 12}
+  produce.each do |item, quantity|
+    puts "There are #{quantity} #{item} in the fridge."
+    end
   end
-    all_meals = meals.join(", ")
-    puts "In summary: #{all_meals}"
+
+# CONDITIONALS
+
+  def water_status(minutes)
+  if minutes < 7
+    puts "The water is not boiling yet."
+  elsif minutes == 7
+    puts "It's just barely boiling"
+  elsif minutes == 8
+    puts "It's boiling!"
+  else
+    puts "Hot! Hot! Hot!"
+  end
+  return self
+end
+
+# CONDITIONAL LOOPING
+
+  def countdown(counter)
+      while counter > 0
+        puts "The counter is #{counter}"
+        counter = counter - 1
+      end
+      return self
+  end
+
 end
 
 # NOT UNDERSTOOD
@@ -35,10 +70,3 @@ end
   # year = Date.today.y
   # puts "Happy " + today + ", it\’s the " + day_of_year + " day of " + year + "."
   # end
-end
-
-class Butler
-	def open_door(door1)
-	puts "Opening the #{door1}"
-	end
-end
