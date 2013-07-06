de7d897e75e796e951d9661708af78c6fbed059c
Ahmed Omran <aomra015@gmail.com>
2013-07-06 17:07:37 -0400

    delete moved files

--- Ruby_in_100_Minutes/suma.rb
+++
@@ -1,9 +0,0 @@
-class Operation
-	def suma(cifra)
-		if cifra + cifra <= 100
-			puts "Tu cifra #{cifra}, sigue dentro del parámetro"
-		elsif cifra + cifra > 100
-			puts "Nahhhht!! tu #{cifra} no encaja."
-		end
-	end
-end
